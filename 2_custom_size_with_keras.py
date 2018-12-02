bc93b88a95ef0637755c8c6fe2ba2ec417918ef1
Atul Prakash Singh <singh.prakash.atul@hotmail.com>
2018-12-03 00:35:31 +0530

    Single Image Preprocessing with Keras Examples

---
+++ 2_custom_size_with_keras.py
@@ -0,0 +1,23 @@
+# Step-1: Import the Library 
+from keras.preprocessing import image
+
+# Step-2: Set the variables containing Input Image and Output Image Path
+input_image_path = 'input_images/img1.png' # Replace with your Image Path
+output_image_path = 'output_images/img1_custom_size.jpg' # Replace with desired Image Path
+
+# Step-3: Define Custom Image Size
+img_size = (128, 128)
+
+# Step-4: Load the Image
+img_raw = image.load_img(input_image_path, target_size=img_size)
+print(img_raw)
+
+# Step-5: Convert the Image into NumPy Array
+img_arr = image.img_to_array(img_raw)
+# print(img_arr)
+print(img_arr.shape)
+
+# Step-6: Generate the Image from NumPy Array
+img_new = image.array_to_img(img_arr)
+print(img_new)
+img_new.save(output_image_path)
